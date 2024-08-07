878ac4222c04618889ab3f13f56be0aa2e4d9bd8
Jariwala-Rudra <148400563+Jariwala-Rudra@users.noreply.github.com>
2024-08-07 19:01:37 -0400

    Create variable

---
+++ variable.java
@@ -0,0 +1,8 @@
+public class variable {
+    public static void main(String[] args) {
+        int num = 2;
+        final String text = "Hello";
+        System.out.println(num);
+        System.out.println(text);
+    }
+}
