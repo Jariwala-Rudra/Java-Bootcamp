a1035e377bdfc88fcc70aa851182cf4b75254d9f
Jariwala-Rudra <148400563+Jariwala-Rudra@users.noreply.github.com>
2024-08-07 19:45:50 -0400

    Fixed conditional error

--- conditionals.java
+++ conditionals.java
@@ -1,10 +1,10 @@
 public class conditionals {
     public static void main(String[] args) {
         int x = 50;
-        if (x > 100) {
+        if (x < 100) {
             x += 50;
         }
-        if (x < 100) {
+        if (x > 100) {
             x /= 20;
         }
         System.out.println(x);
