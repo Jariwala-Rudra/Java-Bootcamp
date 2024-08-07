c5408f8421c4733628026a46f371d5744a7165ca
Jariwala-Rudra <148400563+Jariwala-Rudra@users.noreply.github.com>
2024-08-07 19:58:52 -0400

    Created comments

---
+++ comments.java
@@ -0,0 +1,6 @@
+public class comments {
+    public static void main(String[] args) {
+        // I would rather be 11 feet tall
+        /* This is because it is easier to be taller than it is to be short. */
+    }
+}
