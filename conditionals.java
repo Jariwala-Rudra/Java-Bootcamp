7d3e9abd99bbe990dba0f6fffbd23fea00461560
Jariwala-Rudra <148400563+Jariwala-Rudra@users.noreply.github.com>
2024-08-07 19:42:02 -0400

    Create conditionals

---
+++ conditionals.java
@@ -0,0 +1,12 @@
+public class conditionals {
+    public static void main(String[] args) {
+        int x = 50;
+        if (x > 100) {
+            x += 50;
+        }
+        if (x < 100) {
+            x /= 20;
+        }
+        System.out.println(x);
+    }
+}
