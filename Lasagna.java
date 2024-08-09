6c01e291092471f8956f466bace66cb27a687582
Jariwala-Rudra <148400563+Jariwala-Rudra@users.noreply.github.com>
2024-08-09 19:30:11 -0400

    Created more methods

--- Lasagna.java
+++ Lasagna.java
@@ -1,6 +1,12 @@
 public class Lasagna {
     int expectedMinutesInOven = 40;
-    public void expectedMinutesInOven() {
-        System.out.println("Expected Time in Oven" + expectedMinutesInOven);
+    public int expectedMinutesInOven() {
+        return expectedMinutesInOven;
+    }
+    public int remainingMinutesInOven(int actualMinutesInOven) {
+        return expectedMinutesInOven - actualMinutesInOven;
+    }
+    public int preparationTimeInMins (int amtOfLayers) {
+        return amtOfLayers * 2;
     }
 }
