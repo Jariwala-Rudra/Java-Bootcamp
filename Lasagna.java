538cbb31776b36cdb77c6178f0418625121b0787
Jariwala-Rudra <148400563+Jariwala-Rudra@users.noreply.github.com>
2024-08-09 19:15:54 -0400

    fixed lasagna class errors

--- Lasagna.java
+++ Lasagna.java
@@ -1,6 +1,6 @@
 public class Lasagna {
     int expectedMinutesInOven = 40;
     public void expectedMinutesInOven() {
-        System.out.println(expectedMinutesInOven);
+        System.out.println("Expected Time in Oven" + expectedMinutesInOven);
     }
 }
